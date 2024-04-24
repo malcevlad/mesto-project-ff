2570650e79c5cc479bb2924c5821bac13d11cac1
Vladislav Maltsev <malcelvad777@yandex.ru>
2024-04-25 00:17:53 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+
+function addCard(card, deleteCard) {
+    
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
+    
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+    
+    return cardElement;
+}
+
+const deleteCard = card => card.remove();
+
+
+initialCards.forEach(item => placesList.append(addCard(item, deleteCard)));
