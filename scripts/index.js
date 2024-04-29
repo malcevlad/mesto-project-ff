bc079bbb81a0369b53ca774813e8d957fbe23a77
Vladislav Maltsev <157138049+malcevlad@users.noreply.github.com>
2024-04-29 19:11:45 +0300

    Update index.js Deleted comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,3 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
